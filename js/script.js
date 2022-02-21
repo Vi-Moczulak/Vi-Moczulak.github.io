d8d4ce8691cca94342c08dc2078359ac65363ff6
Vi-Moczulak <69606317+Vi-Moczulak@users.noreply.github.com>
2022-02-21 23:35:24 +0100

    week

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function set_week() {
+    let now = new Date();
+    let onejan = new Date(now.getFullYear(), 0, 1);
+    let week = Math.ceil((((now.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7);
+
+    console.log(week);
+    const isWeekEven = !(week % 2 === 0);
+    document.getElementById("xxx").innerHTML = "Tydzień "+ (isWeekEven? 'parzysty': 'nieparzysty' );
+}
+set_week();
